fa4090bb944056eb97c77cd7688560919301886c
Jondrewd <jonas220703@gmail.com>
2024-04-19 09:51:54 -0300

    Update DaoFactory.java

--- src/daoImplements/DaoFactory.java
+++ src/daoImplements/DaoFactory.java
@@ -1,10 +1,14 @@
 package daoImplements;
 
+import dao.DepartmentDao;
 import dao.SellerDao;
 import db.DB;
 
 public class DaoFactory {
     public static  SellerDao createSellerDao(){
         return new SellerDaoJBDC(DB.getConnection());
+    }
+    public static  DepartmentDao createDepartmentDao(){
+        return new DepartmentDaoJBDC(DB.getConnection());
     }
 }
